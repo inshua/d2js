a0389003bf51f759d0e06320214842ff64beb8b7
inshua <inshua@gmail.com>
2017-05-10 16:46:06 +0800

    add polyfill of Object.assign

--- WebContent/WEB-INF/jslib/lang.js
+++ WebContent/WEB-INF/jslib/lang.js
@@ -133,6 +133,31 @@ Object.defineProperty(Object.prototype, 'merge', {value: function(b, override){
 	return this;
 }, enumerable: false});
 
+if (typeof Object.assign != 'function') {
+  Object.assign = function(target, varArgs) { // .length of function is 2
+    if (target == null) { // TypeError if undefined or null
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    var to = Object(target);
+
+    for (var index = 1; index < arguments.length; index++) {
+      var nextSource = arguments[index];
+
+      if (nextSource != null) { // Skip over if undefined or null
+        for (var nextKey in nextSource) {
+          // Avoid bugs when hasOwnProperty is shadowed
+          if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+            to[nextKey] = nextSource[nextKey];
+          }
+        }
+      }
+    }
+    return to;
+  };
+}
+
+
 var JsTypeUtil = Java.type('org.siphon.common.js.JsTypeUtil');
 /**
  * 将本对象转为Java对象，为 JsTypeUtil.jsObjectToJava 的包装。
